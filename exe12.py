072bcec57aa860ba02a77acbddcff37b7f0b6c4d
MatheusHungaro <40831926+MatheusHungaro@users.noreply.github.com>
2024-10-08 20:35:58 -0300

    Add files via upload

--- exe12.py
+++ exe12.py
@@ -3,10 +3,6 @@ num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 num3 = float(input("Digite o terceiro número: "))
 
-# Verifica qual é o maior número usando if, elif e else
-if num1 > num2 and num1 > num3:
-    print(f"O maior número é: {num1}")
-elif num2 > num1 and num2 > num3:
-    print(f"O maior número é: {num2}")
-else:
-    print(f"O maior número é: {num3}")
+maior = max(num1, num2, num3)
+
+print(f"O maior número é {maior}")
